fde18417e4e3c6e26e67815b994162d7c69a2259
hong-yh <hyinghui@umich.edu>
2019-11-03 05:40:18 -0500

    Hash of data completed

--- ARFurnitureApp/app/src/main/java/com/bongjlee/arfurnitureapp/ProductSubmissionForm.java
+++ ARFurnitureApp/app/src/main/java/com/bongjlee/arfurnitureapp/ProductSubmissionForm.java
@@ -5,50 +5,54 @@ import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
-import com.bongjlee.arfurnitureapp.data.Product;
-import com.google.firebase.firestore.FirebaseFirestore;
-
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.bongjlee.arfurnitureapp.data.Product;
+import com.google.firebase.firestore.FirebaseFirestore;
 
 
 public class ProductSubmissionForm extends AppCompatActivity {
     private static final String TAG = ProductSubmissionForm.class.getSimpleName();
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_product_submission);
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_product_submission );
 //        Intent intent = getIntent();
 //        String value = intent.getStringExtra("toProductSub");
     }
-    public void sendInfo(View view){
+
+    public void sendInfo( View view ) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
 
-        Product product             = new Product();
-        EditText productName        = findViewById(R.id.product_name);
-        EditText productDescription = findViewById(R.id.product_description);
-        EditText productPrice       = findViewById(R.id.product_price);
-        EditText productStyles      = findViewById(R.id.product_styles);
-        EditText shippingInfo       = findViewById(R.id.shipping_info);
+        Product product = new Product();
+        String productName = ( (EditText) findViewById( R.id.product_name ) ).getText().toString();
+        String productDescription = ( (EditText) findViewById( R.id.product_description ) ).getText().toString();
+        String productPrice = ( (EditText) findViewById( R.id.product_price ) ).getText().toString();
+        String productStyles = ( (EditText) findViewById( R.id.product_styles ) ).getText().toString();
+        String shippingInfo = ( (EditText) findViewById( R.id.shipping_info ) ).getText().toString();
 
-        product.setProductName(productName.getText().toString());
-        product.setProductDescription(productDescription.getText().toString());
-        product.setProductPrice(productPrice.getText().toString());
-        product.setProductStyles(productStyles.getText().toString());
-        product.setShippingInfo(shippingInfo.getText().toString());
+        product.setProductName( productName );
+        product.setProductDescription( productDescription );
+        product.setProductPrice( productPrice );
+        product.setProductStyles( productStyles );
+        product.setShippingInfo( shippingInfo );
 
         //Submit data into FireBase cloud
-        db.collection("products")
-                .add(product)
+        db.collection( "products" )
+                .document(
+                        String.valueOf( productName.hashCode() )
+                )
+                .set( product )
                 .addOnSuccessListener(
                         documentReference ->
                                 Log.d(
                                         TAG,
-                                        "DocumentSnapshot added with ID: " + documentReference.getId()
+                                        "DocumentSnapshot added with ID: " + documentReference.toString()
                                 )
                 )
                 .addOnFailureListener(
-                        e -> Log.w(TAG, "Error adding document", e)
+                        e -> Log.w( TAG, "Error adding document", e )
                 );
 
     }
