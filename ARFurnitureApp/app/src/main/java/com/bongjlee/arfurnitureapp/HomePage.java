fd3795b4b55318b8b696401cbbb5eaa80d4765d7
chanyen <ramseyzhan@sjtu.edu.cn>
2019-11-25 13:35:45 -0500

    fix temp

--- ARFurnitureApp/app/src/main/java/com/bongjlee/arfurnitureapp/HomePage.java
+++ ARFurnitureApp/app/src/main/java/com/bongjlee/arfurnitureapp/HomePage.java
@@ -103,15 +103,12 @@ public class HomePage extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
 
-//        TODO change it back
 
-        inflater.inflate(R.menu.post_menu, menu);
-
-//        if (loggedInUser != null) {
-//            inflater.inflate(R.menu.post_menu, menu);
-//        } else {
-//            inflater.inflate(R.menu.pre_menu, menu);
-//        }
+       if (loggedInUser != null) {
+           inflater.inflate(R.menu.post_menu, menu);
+       } else {
+           inflater.inflate(R.menu.pre_menu, menu);
+       }
         return true;
     }
     public boolean onOptionsItemSelected(MenuItem item) {
